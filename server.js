5ca3500c8034cfeaa46157ab98a0617281a474b2
snehasish-roy1303 <66108556+snehasish-roy1303@users.noreply.github.com>
2022-07-12 16:12:19 +0530

    Add to cart functionality

--- server.js
+++ server.js
@@ -1,33 +1,65 @@
+require('dotenv').config()
 const express=require('express')
 const app=express()
 const ejs=require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT= process.env.PORT || 3000
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')
+
+// Database connection
+
+const url = 'mongodb://localhost/pizza-test';
+mongoose.connect(url);
+const connection = mongoose.connection;
+connection.once('open', () => {
+console.log('Database connected...');
+}).on('error',(error)=> {
+    console.log('Connection failed...');
+});
+
+//Session Store
+/*let mongoStore = new MongoDbStore({
+    mongooseConnection: connection, //Connection will be established.
+    collection: 'sessions'  //The table will be created in side as sessions. 
+})*/
+
+//Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoDbStore.create({
+        client: connection.getClient()
+    }),
+    saveUninitialized: false,
+    cookie: {maxAge: 1000*60*60*24}//Calculate in miliseconds and it will give the result of 24 hours, means the cookies will be available till 24 hours
+}))
+
+//Using flash
+app.use(flash())
+
 
 //assets
 app.use(express.static('public'))
+app.use(express.json())
+
+//Global middleware
+app.use((req, res, next)=>{
+    res.locals.session = req.session
+    next()
+})
 
 //Set template engine
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render('home')
-})
+require('./routes/web')(app)
 
-app.get('/cart',(req,res)=>{
-    res.render('customers/cart')
-})
 
-app.get('/login',(req,res)=>{
-    res.render('auth/login')
-})
-
-app.get('/register',(req,res)=>{
-    res.render('auth/register')
-})
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`)
