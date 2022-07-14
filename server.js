dacf7ca4289da0289f68044a6f409899daabe52d
snehasish-roy1303 <66108556+snehasish-roy1303@users.noreply.github.com>
2022-07-14 15:11:37 +0530

    Registration and login done

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
+const passport = require('passport')
 
 // Database connection
 
@@ -20,6 +21,8 @@ console.log('Database connected...');
 }).on('error',(error)=> {
     console.log('Connection failed...');
 });
+
+
 
 //Session Store
 /*let mongoStore = new MongoDbStore({
@@ -41,14 +44,21 @@ app.use(session({
 //Using flash
 app.use(flash())
 
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 
 //assets
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 //Global middleware
 app.use((req, res, next)=>{
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
