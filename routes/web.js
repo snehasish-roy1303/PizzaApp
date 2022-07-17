c5f40fbb4801c7d8e88d7e6c90084328b6a45d76
snehasish-roy1303 <66108556+snehasish-roy1303@users.noreply.github.com>
2022-07-17 13:45:47 +0530

    Problem in admin

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,11 @@
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const orderController = require('../app/http/controllers/customers/orderController')
 const homeController = require('../app/http/controllers/homeController')
 const guest = require('../app/http/middleware/guest')
+const auth = require('../app/http/middleware/auth')
+const AdminorderController = require('../app/http/controllers/admin/orderController')
+
 
 function initRoutes(app){
     app.get('/', homeController().index)
@@ -13,6 +17,13 @@ function initRoutes(app){
 
     app.get('/cart',cartController().cart)
     app.post('/update-cart', cartController().update)
+
+    //Customer routes
+    app.post('/orders', auth, orderController().store)
+    app.get('/customers/orders',auth, orderController().index)
+
+    //Admin routes
+    app.get('/admin/orders', auth, AdminorderController().index)
 }
 
 module.exports = initRoutes
